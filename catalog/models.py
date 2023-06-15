a34cc779329c2d47e960bd200d919cd8d4fe5ef6
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-15 18:04:30 +0200

    Update model Book

--- catalog/models.py
+++ catalog/models.py
@@ -21,6 +21,7 @@ class Book(models.Model):
                                       '">ISBN number</a>')
 
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
