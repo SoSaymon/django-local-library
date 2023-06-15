d77e726bc4902ddf599df8766d82b9c8f3a7bcc1
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-15 19:22:48 +0200

    Add admin register classes in admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
