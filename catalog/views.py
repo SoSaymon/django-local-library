0e4b5a8d1178cb7890a51d99560f70060d03d885
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-16 18:39:39 +0200

    Add genres counter, and books with word "love" in title counter

--- catalog/views.py
+++ catalog/views.py
@@ -9,11 +9,16 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_genres = Genre.objects.all().count()
+    num_books_with_love_word = Book.objects.filter(title__contains="Love").count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_love_word': num_books_with_love_word,
     }
 
     return render(request, 'index.html', context=context)
