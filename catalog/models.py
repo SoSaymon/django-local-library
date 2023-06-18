ae4dfad5577f383c4de2142b1b1c60d20a25ba13
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-18 18:26:04 +0200

    Add authentication, loan list

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 import uuid
+from datetime import date
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Genre(models.Model):
@@ -42,6 +44,8 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
@@ -59,6 +63,11 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (('can_mark_returned', 'Set book as returned'),)
+
+    @property
+    def is_overdue(self):
+        return bool(self.due_back and date.today() > self.due_back)
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
